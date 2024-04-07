20a5a297a50a2db0df121ee3620402edd5dc99c6
Wei-Jan Chang <cc9868422@gmail.com>
2024-04-07 23:06:41 +0200

    clinical data normal range

---
+++ src/data_normal_range.py
@@ -0,0 +1,24 @@
+__all__ = {'DATA_NORMAL_RANGE'}
+DATA_NORMAL_RANGE = {
+    "preop_ecg": 'Normal Sinus Rhythm',  # type:str
+    "preop_pft": 'Normal',  # type: str
+    "preop_hb": (13, 17),  # type: int  # g/dL
+    "preop_plt": (130, 400),  # type: int  # x1000/mcL
+    "preop_pt": (80, 120),  # type: int  # %
+    "preop_aptt": (26.7, 36.6),  # type: int  # sec
+    "preop_na": (135, 145),  # type: int  # mmol/L
+    "preop_k": (3.5, 5.5),  # type: int  # mmol/L
+    "preop_gluc": (70, 110),  # type: int  # mg/dL
+    "preop_alb": (3.3, 5.2),  # type: int  # g/dL
+    "preop_ast": (1, 40),  # type: int  # IU/L
+    "preop_alt": (1, 40),  # type: int  # IU/L
+    "preop_bun": (10, 26),  # type: int  # mg/dL
+    "preop_cr": (0.70, 1.40),  # type: int  # mg/dL
+    "preop_ph": (7.35, 7.45),  # type: int
+    "preop_hco3": (18, 23),  # type: int  # mmol/L
+    "preop_be": (-2.0, 3.0),  # type: int  # mmol/L
+    "preop_pao2": (83, 108),  # type: int  # mmHg
+    "preop_paco2": (35, 48),  # type: int  # mmHg
+    "preop_sao2": (95, 98),  # type: int  # %
+}
+
