92b5ef82257227095ed8a721873447b509c84a26
Wei-Jan Chang <98678790+WeiJanChang@users.noreply.github.com>
2024-12-03 13:52:02 +0000

    solve warning

--- src/visualisation.py
+++ src/visualisation.py
@@ -18,7 +18,7 @@ def bar_chart(df: pd.DataFrame, age_group: str = None, gender: str = None):
         labels = ['Toddler (0-3)', 'Child (3-12)', 'Teen (12-19)', 'Adult (19-64)', 'Older people (64-110)']
         df["age group"] = pd.cut(df.age, bins=bins, labels=labels, right=False)
         if gender is not None:
-            gender_counts = df.groupby(['age group', 'sex']).size().unstack(fill_value=0)
+            gender_counts = df.groupby(['age group', 'sex'], observed=False).size().unstack(fill_value=0)
             gender_counts.plot(kind='bar', stacked=False, color=['blue', 'red'], alpha=0.7)
 
         plt.title('Gender Distribution by Age Group')
@@ -29,10 +29,3 @@ def bar_chart(df: pd.DataFrame, age_group: str = None, gender: str = None):
         plt.legend(title='Gender')
 
     return plt.show()
-
-
-if __name__ == '__main__':
-    anes_df = pd.read_csv("/Users/wei/Documents/physionet_surgicalpatients/clinical_data.csv")
-    age_group = True
-    gender = True
-    bar_chart(anes_df, age_group, gender)
