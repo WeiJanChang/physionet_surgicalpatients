b5d52214a9035819e28037b4813a1eea8d61b9b2
Wei-Jan Chang <98678790+WeiJanChang@users.noreply.github.com>
2024-12-08 14:13:25 +0000

    barchart or piechart with count label

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,5 +1,8 @@
+import pandas
 import pandas as pd
 import matplotlib.pyplot as plt
+
+__all__ = [""]
 
 
 # todo create dashboard by age by gender by op by dx
@@ -34,18 +37,23 @@ def bar_chart(df: pd.DataFrame, age_group: str = None, gender: str = None):
     return plt.show()
 
 
-def bar_chart_by_gender(df: pd.DataFrame,
-                        filter_col: str,
-                        apply_filter: bool = True,
-                        filter_val: str | int = None):
+def figure_by_gender(df: pd.DataFrame,
+                     filter_col: str,
+                     apply_filter: bool = True,
+                     bar_chart: bool = False,
+                     pie_chart: bool = False,
+                     filter_val: str | int = None):
     """
     Generates a bar chart showing gender distribution for a specific column.
+
 
     :param df: dataframe
     :param filter_col: column name to filter the data
     :param apply_filter: whether to filter the df based on the 'filter_value'
     :param filter_val:value to filter the filter_column
-    :return: bar chart
+    :param bar_chart: bar chart
+    :param pie_chart: pie chart
+    :return: plot
     """
     df = df.copy()
     if apply_filter:
@@ -54,17 +62,18 @@ def bar_chart_by_gender(df: pd.DataFrame,
         df = df[df[filter_col] == filter_val]
 
     # grouping data and create bar chart
-
-    gender_counts = df.groupby(['sex']).size()
-
-    gender_counts.plot(kind='bar', color=['blue', 'red'], alpha=0.7)
+    gender_counts: pandas.Series = df.groupby(['sex']).size()
+    if bar_chart:
+        # Apply a function groupby to each row or column of a DataFrame.
+        # size(): Compute group sizes
+        gender_counts.plot(kind='bar', color=['blue', 'red'], alpha=0.7, )
+    if pie_chart:
+        gender_counts.plot(kind='pie', color=['blue', 'red'],
+                           autopct=lambda pct: f'{int(pct / 100. * gender_counts.sum())} ({pct:.1f}%)',
+                           labels=gender_counts.index)
 
     plt.xlabel("Gender")
     plt.ylabel("Count")
+    plt.xticks(rotation=0)  # adjust x-axis label rotation
     plt.title(f"Gender distribution of {filter_col} in {filter_val}")
     plt.show()
-
-
-if __name__ == '__main__':
-    df = pd.read_csv('~/code/physionet_surgicalpatients/test_file/clinical_data.csv')
-    bar_chart_by_gender(df, filter_col='opname', filter_val='Low anterior resection')
